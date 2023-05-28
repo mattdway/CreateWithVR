73edc17ea22df8ea230551bd0fbce035a0f01bc8
mattdway <91771825+mattdway@users.noreply.github.com>
2023-05-28 04:55:31 -0600

    05/28/23 Commit
    More changes and adjustments made including two layer pieces and two inspector linking pieces that were causing errors with the XR Jedi Grabbable script.  Also changed the line interactor enable event from Hover Exit to Select Exit Last so that the ray is only enabled when the item has been dropped.
    
    Fixed the brick shader to not be transparent, moved it slightly and set its mass higher so it has a better chance of breaking the window.
    
    Still need to play test everything thoroughly.

--- Assets/Scripts/Animated Physics Hands/ShakeEffect.cs
+++ Assets/Scripts/Animated Physics Hands/ShakeEffect.cs
@@ -5,6 +5,9 @@ public class ShakeEffect : MonoBehaviour
 {
     public float shakeDuration = 0.5f;
     public float shakeIntensity = 0.1f;
+    public bool shakeOnX = true;
+    public bool shakeOnY = true;
+    public bool shakeOnZ = false; // Set to true if you want shaking on the Z-axis as well
 
     private Vector3 originalPosition;
 
@@ -24,10 +27,11 @@ public class ShakeEffect : MonoBehaviour
 
         while (elapsed < shakeDuration)
         {
-            float offsetX = Random.Range(-1f, 1f) * shakeIntensity;
-            float offsetY = Random.Range(-1f, 1f) * shakeIntensity;
+            float offsetX = shakeOnX ? Random.Range(-1f, 1f) * shakeIntensity : 0f;
+            float offsetY = shakeOnY ? Random.Range(-1f, 1f) * shakeIntensity : 0f;
+            float offsetZ = shakeOnZ ? Random.Range(-1f, 1f) * shakeIntensity : 0f;
 
-            transform.localPosition = originalPosition + new Vector3(offsetX, offsetY, 0f);
+            transform.localPosition = originalPosition + new Vector3(offsetX, offsetY, offsetZ);
 
             elapsed += Time.deltaTime;
             yield return null;
